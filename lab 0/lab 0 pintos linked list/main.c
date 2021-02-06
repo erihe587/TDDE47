e124387d38eceb311f605cff87d5296804e00e7c
Alexander Skoglund <64963344+alesk852@users.noreply.github.com>
2021-02-06 12:06:16 +0100

    Update main.c

--- lab 0/lab 0 pintos linked list/main.c
+++ lab 0/lab 0 pintos linked list/main.c
@@ -12,17 +12,15 @@ void insert(struct list *student_list) {
 
 	char *student_name = (char*) malloc(50*sizeof(char));
 
-
 	printf("Name: ");
 	scanf("%s", student_name);
 	struct student *new = (struct student *)malloc(sizeof(struct student));
 	new->name = student_name;
-	//printf("%s \n", (list_entry(&new->elem, struct student, elem))->name);
 	list_push_back(student_list, &new->elem);
 }
 
 void delete(struct list *student_list) {
-	
+
 	printf("Name: ");
     char student_name[255];
     scanf("%s",student_name);
@@ -31,24 +29,23 @@ void delete(struct list *student_list) {
 	for(e = list_begin(student_list); e != list_end(student_list); e = list_next(e)) {
 		struct student *current_student = list_entry (e, struct student, elem);
 		if(strcmp((list_entry (e, struct student, elem))->name, student_name) == 0) {
+			list_remove(e);
 			free(list_entry(e, struct student, elem)->name);
 			free(list_entry(e, struct student, elem));
-			list_remove(e);
 			return;
 		}
-	} 
+	}
+	printf("The student is not on the list \n");
 }
 
 void list(struct list *student_list) {
-	
+
 	if(!list_empty(student_list)) {
 
 		struct list_elem *e;
   	for(e = list_begin(student_list); e != list_end(student_list); e = list_next(e)) {
     	struct student *current_student = list_entry(e, struct student, elem);
-		printf("%s ", current_student->name);
-    	//printf("%s ", list_entry(e, struct student, elem)->name);
-
+			printf("%s ", current_student->name);
   	}
 	}else{
 		printf("The list is empty");
@@ -57,9 +54,10 @@ void list(struct list *student_list) {
 }
 
 void quit(struct list *student_list) {
-	
+
 	while (!list_empty(student_list)) {
 		struct list_elem *e = list_pop_front(student_list);
+		free(list_entry(e, struct student, elem)->name);
 		free(list_entry(e, struct student, elem));
 		}
 
